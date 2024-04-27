e9a6afbf627ac9f67d5824242da20b9d6f860b5f
soilihi laydi <soilihi.laydi@gmail.com>
2024-04-27 04:07:08 +0200

    creation serveur backend

--- api/src/app.js
+++ api/src/app.js
@@ -1,6 +1,6 @@
-require('dotenv').config();
-const express = require('express');
-const cors = require('cors');
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
 
 const app = express();
 
