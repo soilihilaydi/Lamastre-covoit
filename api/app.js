cafcba16bdd94564c3e196e3cc8b27daccf04b94
soilihi laydi <soilihi.laydi@gmail.com>
2024-04-30 14:23:28 +0200

    amelioration architecture

---
+++ api/app.js
@@ -0,0 +1,14 @@
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+
+// Middlewares
+app.use(cors());
+app.use(express.json());
+
+// Routes
+// TODO: Ajouter les routes ici
+
+module.exports = app;
